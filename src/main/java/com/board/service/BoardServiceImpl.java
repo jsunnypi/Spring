88afdfddbdc2a2c0d4343037eac7efcda692a704
jsunnypi <appleorange0314@gmail.com>
2025-01-02 17:03:33 +0900

    board

---
+++ src/main/java/com/board/service/BoardServiceImpl.java
@@ -0,0 +1,61 @@
+package com.board.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.board.mapper.BoardMapper;
+import com.board.model.BoardVO;
+import com.board.model.Criteria;
+
+@Service
+public class BoardServiceImpl implements BoardService {
+
+	@Autowired
+	private BoardMapper mapper;
+	
+	@Override
+	public void enroll(BoardVO board) {
+		
+		mapper.enroll(board);
+		
+	}
+
+	@Override
+	public List<BoardVO> getList() {
+		
+		return mapper.getList();
+		
+	}
+
+	@Override
+	public BoardVO getPage(int bno) {
+
+		return mapper.getPage(bno);
+		
+	}
+
+	@Override
+	public int modify(BoardVO board) {
+
+		return mapper.modify(board);
+		
+	}
+
+	@Override
+	public int delete(int bno) {
+		return mapper.delete(bno);
+	}
+
+	@Override
+	public List<BoardVO> getListPaging(Criteria cri) {
+		return mapper.getListPaging(cri);
+	}
+
+	@Override
+	public int getTotal(Criteria cri) {
+		return mapper.getTotal(cri);
+	}
+
+}
